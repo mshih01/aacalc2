fc1bb53a63f2acb1fa10215978d62465489ca294
Mitchell Shih <mitchell_shih@yahoo.com>
2025-06-07 10:01:09 -0700

    add test for sbrExternal

--- src/external.test.ts
+++ src/external.test.ts
@@ -2,6 +2,8 @@ import {
   type MultiwaveInput,
   type MultiwaveOutput,
   multiwaveExternal,
+  sbrExternal,
+  type SbrInput,
 } from './index.js';
 import { test, expect } from 'vitest';
 
@@ -52,4 +54,36 @@ test('multiwaveExternal', () => {
   expect(output.attack.ipcLoss[0]).to.closeTo(145.518327700801, 1e-6);
   expect(output.defense.survives[0]).to.closeTo(0.2015387892399474, 1e-6);
   expect(output.defense.ipcLoss[0]).to.closeTo(242.3648995989333, 1e-6);
+});
+
+test('sbrExternal', () => {
+  const input: SbrInput = {
+    verbose_level: 0,
+    diceMode: 'standard',
+    attack: {
+      units: {
+        bom: 5,
+      },
+      ool: ['bom'],
+      takes: 0,
+      aaLast: false,
+    },
+    defense: {
+      units: {
+        ic: 20,
+      },
+      ool: ['ic'],
+      takes: 0,
+      aaLast: false,
+    },
+    in_progress: false,
+    reportPruneThreshold: 1e-12,
+    pruneThreshold: 1e-12,
+  };
+  let output = sbrExternal(input);
+
+  expect(output.attack.survives[0]).to.closeTo(0.999871399176955, 1e-6);
+  expect(output.attack.ipcLoss[0]).to.closeTo(10, 1e-6);
+  expect(output.defense.survives[0]).to.closeTo(0.8556527702363717, 1e-6);
+  expect(output.defense.ipcLoss[0]).to.closeTo(14.329007261845037, 1e-6);
 });
