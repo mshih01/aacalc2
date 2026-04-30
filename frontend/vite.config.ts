9efa1c90908d50a495576f4328f4b1ce90ce95c7
mshih01 <mitchell_shih@yahoo.com>
2026-04-30 15:35:53 -0700

    vite allow LOCAL_ENGINE=1 npm run dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const localEngine = process.env.LOCAL_ENGINE
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/aacalc2/',
   plugins: [react()],
+  resolve: localEngine ? {
+    alias: {
+      aacalc2: path.resolve(__dirname, '..', 'dist'),
+    },
+  } : undefined,
   build: {
     outDir: 'dist',
   }
